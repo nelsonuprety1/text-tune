5c3738b2c9c19c232f732f7090622f9067be5111
Nelson Uprety <nelsonuprety3@gmail.com>
2023-03-07 17:06:35 +0000

    added components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import Home from './components/Home/Home';
 import Navbar from './pages/Navbar';
-
+import { Routes, Route } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
     </div>
   );
 }
