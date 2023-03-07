2064da3f49b67020e3d50f387760b4ac3bb5f846
Nelson Uprety <nelsonuprety3@gmail.com>
2023-03-07 10:34:06 +0000

    first commit

--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom';
 const Navbar = () => {
   return (
     <nav className="main-nav">
-      {/* <NavLink to="/">Home</NavLink> */}
+      <NavLink to="/">Home</NavLink>
       <NavLink to="/convert">Text Converter</NavLink>
       <NavLink to="/similar">Synonyms & Antonyms</NavLink>
     </nav>
