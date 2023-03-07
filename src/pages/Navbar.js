43120ddd3f18d16230fc8b2c0f649ac4c68b3098
Nelson Uprety <nelsonuprety3@gmail.com>
2023-03-07 10:33:18 +0000

    start of project

---
+++ src/pages/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+const Navbar = () => {
+  return (
+    <nav className="main-nav">
+      {/* <NavLink to="/">Home</NavLink> */}
+      <NavLink to="/convert">Text Converter</NavLink>
+      <NavLink to="/similar">Synonyms & Antonyms</NavLink>
+    </nav>
+  );
+};
+
+export default Navbar;
