3d8a0f5fee0d72091985d009d3e7cd01c4e4a8fc
Nelson Uprety <nelsonuprety3@gmail.com>
2023-03-09 02:39:02 +0000

    changed some routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import Home from './components/Home/Home';
 import Navbar from './pages/Navbar';
+import Converter from './components/Converter/Converter';
+import Similar from './components/Similar/Similar';
 import { Routes, Route } from 'react-router-dom';
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />}></Route>
+        <Route path="/convert" element={<Converter />}></Route>
+        <Route path="/similar" element={<Similar />}></Route>
       </Routes>
     </div>
   );
