256df40a1203dfe84d65203db22df2a1bc1c2358
Nelson Uprety <nelsonuprety3@gmail.com>
2023-03-09 02:38:50 +0000

    similar component

---
+++ src/components/Similar/Similar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './similar.css';
+const Similar = () => {
+  return (
+    <div>
+      <h2>
+        Choose your words wisely. Our synonym and anotymn finder helps you find
+        the perfect ones. Upgrade your writing today.”
+      </h2>
+    </div>
+  );
+};
+
+export default Similar;
