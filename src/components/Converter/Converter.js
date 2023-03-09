0f09747f11345f59801d2d5f9ddd6abb730b7c6f
Nelson Uprety <nelsonuprety3@gmail.com>
2023-03-09 11:29:54 +0000

    Functionality almost done

--- src/components/Converter/Converter.js
+++ src/components/Converter/Converter.js
@@ -1,8 +1,66 @@
 import './converter.css';
 
-import React from 'react';
+import React, { useState } from 'react';
 
 const Converter = () => {
+  const [textContent, setTextContent] = useState('');
+  const [wordCount, setWordCount] = useState(0);
+  const [charCount, setCharCount] = useState(0);
+
+  const handlingButtonClick = text => {
+    let changedText;
+    switch (text) {
+      case 'uppercasing':
+        changedText = textContent.toUpperCase();
+        break;
+      case 'lowercasing':
+        changedText = textContent.toLowerCase();
+        break;
+      case 'titlecasing':
+        /**
+         * In this code
+         -- split(' ') it splits the input string into an array of words
+
+         -- .map() it loops over each word in the array, and it applied a function
+         that capitalized the first letter of the word and converted rest of the letters to lowercase
+
+         -- .slice(1) method is used to get a substring of word starting 
+         from the second character, which removes the first character of the word.
+         eg: if word is 'eminem' , word.slice(1) returns 'minem
+
+         -- .join(' ') combined the modified words back into a sing string,with each word
+         separated by the space. 
+         */
+        changedText = textContent
+          .split(' ')
+          .map(
+            word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
+          )
+          .join(' ');
+        break;
+      case 'reversing':
+        changedText = textContent.split('').reverse().join('');
+        break;
+      case 'removewhitespace':
+        changedText = textContent.split(' ').join('');
+        break;
+      case 'copy':
+        navigator.clipboard.writeText(textContent);
+        changedText = textContent;
+        break;
+      case 'clear':
+        changedText = '';
+        break;
+      default:
+        changedText = '';
+    }
+    setTextContent(changedText);
+  };
+
+  const handlingTextChange = event => {
+    setTextContent(event.target.value);
+  };
+
   return (
     <div className="container">
       <h1 className="heading">
@@ -17,19 +75,50 @@ const Converter = () => {
         <p className="words">Words:</p>
         <p className="characters">Characters: </p>
         <textarea
+          value={textContent}
+          onChange={handlingTextChange}
           name="contentBox"
           id="contentBox"
           placeholder="Type or paste your text"
         ></textarea>
       </div>
       <div className="buttons">
-        <button className="btns">UPPERCASE</button>
-        <button className="btns">lowercase</button>
-        <button className="btns">Title Case</button>
-        <button className="btns">Reverse Text</button>
-        <button className="btns">Remove WhiteSpace</button>
-        <button className="btns">Copy to Clipboard</button>
-        <button className="btns">Clear</button>
+        <button
+          className="btns"
+          onClick={() => handlingButtonClick('uppercasing')}
+        >
+          UPPERCASE
+        </button>
+        <button
+          className="btns"
+          onClick={() => handlingButtonClick('lowercasing')}
+        >
+          lowercase
+        </button>
+        <button
+          className="btns"
+          onClick={() => handlingButtonClick('titlecasing')}
+        >
+          Title Case
+        </button>
+        <button
+          className="btns"
+          onClick={() => handlingButtonClick('reversing')}
+        >
+          Reverse Text
+        </button>
+        <button
+          className="btns"
+          onClick={() => handlingButtonClick('removewhitespace')}
+        >
+          Remove WhiteSpace
+        </button>
+        <button className="btns" onClick={() => handlingButtonClick('copy')}>
+          Copy to Clipboard
+        </button>
+        <button className="btns" onClick={() => handlingButtonClick('clear')}>
+          Clear
+        </button>
       </div>
     </div>
   );
