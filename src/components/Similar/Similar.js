a5d8dc0790cea0bc3b49c1b62293041062093701
Nelson Uprety <nelsonuprety3@gmail.com>
2023-03-10 11:18:48 +0000

    Similar design almost completed

--- src/components/Similar/Similar.js
+++ src/components/Similar/Similar.js
@@ -2,11 +2,24 @@ import React from 'react';
 import './similar.css';
 const Similar = () => {
   return (
-    <div>
-      <h2>
-        Choose your words wisely. Our synonym and anotymn finder helps you find
-        the perfect ones. Upgrade your writing today.”
-      </h2>
+    <div className="similar-content">
+      <h1 className="main-heading">Choose your words wisely.</h1>
+      <p className="contents">
+        Our synonym and antonym finder helps you find the perfect ones. Upgrade
+        your writing today.”
+      </p>
+      <div className="content-box">
+        <label htmlFor="similarity">Please Enter one word at a time</label>
+        <input id="similarity" type="text"></input>
+        <div className="buttons">
+          <button className="btns">Synonym</button>
+          <button className="btns">Antonym</button>
+          <button className="btns">Copy to Clipboard</button>
+        </div>
+      </div>
+      <div className="textbox">
+        <textarea name="similarbox" id="similarbox" readOnly></textarea>
+      </div>
     </div>
   );
 };
